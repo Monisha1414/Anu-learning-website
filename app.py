b8e99b8708ae8f4e267b28a33c73836d98ba83eb
Monisha1414 <128897586+Monisha1414@users.noreply.github.com>
2023-05-11 12:08:06 +0000

    new updates for login page

--- app.py
+++ app.py
@@ -8,5 +8,24 @@ def hello_world():
   return render_template('home.html')
 
 
+database = {
+  'GeeksForGeeks': '123',
+  'Abdul Kalam': 'xyz',
+  'Jony': 'abc',
+  'Tony': 'pqr'
+}
+
+
+@app.route('/form_login', methods=['POST', 'GET'])
+def home():
+  uname = request.form['username']
+  password = request.form['password']
+  if uname not in database:
+    return render_template('home.html', info='Invalid User ????!')
+  else:
+    if database[uname] != password:
+      return render_template('home.html', info='Invalid Password ????!')
+
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
